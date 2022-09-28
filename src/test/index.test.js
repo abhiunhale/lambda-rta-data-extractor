8052e7433309d53c5cdee50a63d0691910aaa8bd
dagrawal <disha.agrawal@nice.com>
2022-09-28 16:10:40 +0530

    CXWFM-31108-sample unit test case was added

--- src/test/index.test.js
+++ src/test/index.test.js
@@ -4,28 +4,31 @@ let expect = require('chai').expect;
 let rewire = require("rewire");
 let LambdaTester = require('lambda-tester');
 let mainModule = rewire('../index.js');
-let myHandler = mainModule.handler;
+let Handler = mainModule.handler;
 let Executor = mainModule.Executor;
-let moment = require('moment');
-let request = require('request');
-let AWSMock = require('aws-sdk-mock');
-let sinon = require("sinon");
-let executor;
-let performGetRequestToCXoneStub;
-let LambdaUtils = require('../LambdaUtils.js');
-let token = "token";
-let schemaName = "mock_tenant";
-let host = "http://locallink";
+var mockEvent;
 
-describe('Describe your AWS Lambda functionality here', function () {
-
+describe('WFM RTA export report test', function () {
+    this.timeout(30000);
+    var getMockEvent = function (eventData) {
+        return eventData;
+    };
 
     beforeEach(function () {
-
+        mockEvent = getMockEvent(JSON.parse(JSON.stringify(require('./mocks/mockEvent.json'))));
     });
 
-    afterEach(function () {
+    it("Report export Done with status = 200", done => {
+        LambdaTester(Handler)
+            .event(mockEvent)
+            .expectResult((result) => {
 
+                expect(result.statusCode).to.exist;
+                expect(result.statusCode).to.equal(200);
+                expect(result.message).to.exist;
+                expect(result.message).to.equal("hello world");
+                done();
+            })
+            .catch(done);
     });
-
 });
