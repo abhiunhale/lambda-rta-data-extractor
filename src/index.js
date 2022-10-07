d583ed122d8227a167dee5dd376f311808b79a01
dagrawal <disha.agrawal@nice.com>
2022-10-07 19:53:10 +0530

    CXWFM-31108- authentication, event verification and userhub api integrated

--- src/index.js
+++ src/index.js
@@ -8,23 +8,117 @@ Promise.allSettled = require("promise.allsettled");
 let sizeof = require('object-sizeof');
 let LambdaUtils = require('./LambdaUtils.js');
 let commonUtils = require('lambda-common-utils');
+const logger = commonUtils.loggerUtils.getLogger;
+const USER_HUB_PATH="/user-management/v1/users";
 
 function Executor(event, context) {
-    console.log("in the lambda executor");
+
+    let self = this;
+    let host = process.env.SERVICE_URL;
+    let token = event.evolveAuth.token;
+    let tenant = {};
+
+    self.authenticateRequest = async function () {
+        logger.log('Step - Call TM for getting tenant IC details');
+        await LambdaUtils.performGetRequestToCXone("/tenants/current?sensitive=true", token, host,false).then((response) => {
+                tenant = JSON.parse(response).tenant;
+                logger.log("tenant details: "+ JSON.stringify(tenant));
+        }).catch((error) => {
+            commonUtils.metricsWriter.addTenantMetricWithDimension(tenant, `LMBD-WFM-export-failures-D:reason`, 'getTenant', 1);
+            commonUtils.metricsWriter.flushAggregatedMetrics();
+            logger.error(JSON.stringify(error));
+            throw new Error(JSON.stringify(error));
+        });
+    };
+
+    self.verifyWFMLicense = function () {
+        logger.log('Step - Verify WFM license from tenant details');
+        logger.info("tenant for license verification : "+ JSON.stringify(tenant));
+        if (tenant && tenant.licenses && tenant.licenses.length > 0) {
+            let licenseLen = tenant.licenses.length;
+            for (let i = 0; i < licenseLen; i += 1) {
+                if (tenant.licenses[i].applicationId === "WFM") {
+                    return true;
+                }
+            }
+            return false;
+        }
+        return false;
+    };
+
+    self.verifyEvent = function () {
+        logger.log('Step - Verify event details');
+        logger.info("event for verification : "+ JSON.stringify(event));
+        if (!event || !event.reportName || event.reportName !== "adherenceV2") {
+            return false;
+        }
+        if (!event.reportDateRange || !event.reportDateRange.from || !event.reportDateRange.to) {
+            return false;
+        }
+        if ( !event.query || event.query.length <= 0 ) {
+            return false;
+        }
+        return true;
+    };
+
+    self.getUsersFromUH = async function() {
+        let userIds = [];
+        await LambdaUtils.performGetRequestToCXone(USER_HUB_PATH, token, host,true,tenant.schemaName).then((response) => {
+            let users = JSON.parse(response).users;
+            logger.info("response received from userhub api :"+ users.length);
+            users.forEach(function (user) {
+                userIds.push(user.id);
+            });
+        }).catch((error) => {
+            logger.error("error in getting users from WFO" + error);
+        });
+        return userIds;
+    };
 }
 
 exports.Executor = Executor;
 
 /**
- * Your AWS Lambda description goes here
+ * Lambda to extract WFM data from Snowflake DL
  */
 exports.handler = async (event, context) => {
-    console.log("in the lambda handler function");
+    let response = {};
+    let hasWFMLicense = false;
+    let executor = new Executor(event, context);
+    let failureMessage = "Fail to extract WFM data";
+    commonUtils.loggerUtils.setDebugMode(process.env.DEBUG);
 
-    let response = {
+    logger.log('1. BEGIN HANDLER AND VERIFY HOST');
+    logger.info("event:"+ JSON.stringify(event));
+    logger.info('host from process :' + process.env.SERVICE_URL);
+
+    if (!process.env.SERVICE_URL || process.env.SERVICE_URL == null) {
+        return context.fail("FAILED TO VALIDATE HOST");
+    }
+
+    try {
+        logger.log('2. AUTHENTICATE REQUEST');
+        await executor.authenticateRequest();
+
+        logger.log('3. VERIFY WFM LICENSE');
+        hasWFMLicense = executor.verifyWFMLicense();
+        if(!hasWFMLicense) { return context.fail(failureMessage); }
+
+        logger.log('4. VERIFY REQUEST BODY FOR FILTERS');
+        let isEventValid = executor.verifyEvent();
+        if(!isEventValid) { return context.fail(failureMessage); }
+
+        logger.log('5. GET LIST OF USER IDs');
+        let userIds = await executor.getUsersFromUH();
+
+        logger.log('6. UPLOAD DATA TO S3');
+    } catch (error) {
+        return context.fail(error);
+    }
+
+    response = {
         statusCode: 200,
         message: "hello world"
     };
-    console.log("response: " + JSON.stringify(response));
     return response;
 };
--- src/test/index.test.js
+++ src/test/index.test.js
@@ -2,31 +2,204 @@
 
 let expect = require('chai').expect;
 let rewire = require("rewire");
+let sinon = require("sinon");
 let LambdaTester = require('lambda-tester');
+let LambdaUtils = require('../LambdaUtils.js');
 let mainModule = rewire('../index.js');
 let Handler = mainModule.handler;
 let Executor = mainModule.Executor;
-var mockEvent;
+let executor;
+var mockEvent, performGetRequestToCXoneStub, mockAPIResponse;
 
 describe('WFM RTA export report test', function () {
-    this.timeout(30000);
+    this.timeout(3000);
     var getMockEvent = function (eventData) {
         return eventData;
     };
 
     beforeEach(function () {
         mockEvent = getMockEvent(JSON.parse(JSON.stringify(require('./mocks/mockEvent.json'))));
+        mockAPIResponse = getMockEvent(JSON.parse(JSON.stringify(require('./mocks/mockAPIResult.json'))));
+        process.env.SERVICE_URL = "https://na1.test.nice-incontact.com";
+        performGetRequestToCXoneStub = sinon.stub(LambdaUtils, 'performGetRequestToCXone');
+        executor = new Executor(mockEvent, {});
+    });
+
+    afterEach(function(){
+        performGetRequestToCXoneStub.restore();
     });
 
     it("Report export Done with status = 200", done => {
+        let response = mockAPIResponse;
+        response.users = [];
+        performGetRequestToCXoneStub.resolves(JSON.stringify(response));
         LambdaTester(Handler)
             .event(mockEvent)
             .expectResult((result) => {
-
                 expect(result.statusCode).to.exist;
                 expect(result.statusCode).to.equal(200);
                 expect(result.message).to.exist;
                 expect(result.message).to.equal("hello world");
+                done();
+            })
+            .catch(done);
+    });
+
+    it("Verify executor authenticate request method", async() => {
+        performGetRequestToCXoneStub.resolves(JSON.stringify(mockAPIResponse));
+        let result = await executor.authenticateRequest();
+    });
+
+    it("Verify handler failure while request authentication", done => {
+        let errorMsg = {err: 'Invalid token'};
+        performGetRequestToCXoneStub.rejects(errorMsg);
+        LambdaTester(Handler)
+            .event(mockEvent)
+            .expectError(error => {
+                expect(error.message).to.equal(JSON.stringify(errorMsg));
+                done();
+            })
+            .catch(done);
+    });
+
+    it("Verify executor authenticate request method", done => {
+        let errorMsg = {err: 'Invalid token'};
+        performGetRequestToCXoneStub.rejects(errorMsg);
+        executor.authenticateRequest().catch(error => {
+            expect(error.message).to.equal(JSON.stringify(errorMsg));
+            done();
+        });
+    });
+
+    it("Verify executor method has license returns true", async() => {
+        performGetRequestToCXoneStub.resolves(JSON.stringify(mockAPIResponse));
+        await executor.authenticateRequest();
+        let hasLicense = executor.verifyWFMLicense();
+        expect(hasLicense).to.equal(true);
+    });
+
+    it("Verify executor method returns false when WFM license is not present", async() => {
+        let tenant = { "tenant" : {
+                "licenses" : [ {
+                    "applicationId" : "PLATFORMSERVICES",
+                    "productId" : "EVOLVE",
+                    "featureIds" : [ 141, 62, 111 ],
+                    "settings" : { }
+                }]
+        }};
+        performGetRequestToCXoneStub.resolves(JSON.stringify(tenant));
+        await executor.authenticateRequest();
+        let hasLicense = executor.verifyWFMLicense();
+        expect(hasLicense).to.equal(false);
+    });
+
+    it("Verify executor method returns false when licenses are not present", async() => {
+        let tenant = {"tenant" : { "licenses": [] }};
+        performGetRequestToCXoneStub.resolves(JSON.stringify(tenant));
+        await executor.authenticateRequest();
+        let hasLicense = executor.verifyWFMLicense();
+        expect(hasLicense).to.equal(false);
+    });
+
+    it("Verify executor method returns true when event has valid request", async() => {
+        let isEventValid = executor.verifyEvent();
+        expect(isEventValid).to.equal(true);
+    });
+
+    it("Verify executor method returns false when event has no request", async() => {
+        let invalidEvent = {
+            "evolveAuth": {
+                "token" : "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiJ1c2VyOjExZThjMmQyLTY2OGEtNGFlMC05MzVmLTAyNDJhYzExMDAwMyIsInJvbGUiOnsibGVnYWN5SWQiOiJBZG1pbmlzdHJhdG9yIiwic2Vjb25kYXJ5Um9sZXMiOlt7ImlkIjoiMTFlOTA5MDAtN2NmZS0yMDcwLTkzNzUtMDI0MmFjMTEwMDA1IiwibGFzdFVwZGF0ZVRpbWUiOjE2MTIyMDM0NDEwMDB9XSwiaWQiOiIxMWU4YzJkMi02MzhjLWMyNTAtYjk5NC0wMjQyYWMxMTAwMDIiLCJsYXN0VXBkYXRlVGltZSI6MTY2NTA1MTQ5NDUyN30sImljQWdlbnRJZCI6IjMyNTgxMyIsImlzcyI6Imh0dHBzOlwvXC9hdXRoLnRlc3QubmljZS1pbmNvbnRhY3QuY29tIiwiZ2l2ZW5fbmFtZSI6IkVtaWx5IiwiYXVkIjoiaW5Db250YWN0IEV2b2x2ZUBpbkNvbnRhY3QuY29tIiwiaWNTUElkIjoiMTA0ODIiLCJpY0JVSWQiOjExMjYzMjE1NzgsIm5hbWUiOiJwbS5rZXBsZXIuYWRtaW5pc3RyYXRvckB3Zm9zYWFzLmNvbSIsInRlbmFudElkIjoiMTFlOGMyZDItNWZiZS1jYTYwLTg1MjQtMDI0MmFjMTEwMDA5IiwiZXhwIjoxNjY1MTQzMTQ2LCJpYXQiOjE2NjUxMzk1NDYsImZhbWlseV9uYW1lIjoiU21pdGgiLCJ0ZW5hbnQiOiJwZXJtX3BtX2tlcGxlcl90ZW5hbnQyNDEzNDg0MCIsInZpZXdzIjp7fSwiaWNDbHVzdGVySWQiOiJUTzMyIn0.Z-vcxglWSK83V7w0fxAKSNOjWktdH4FVb1fGPSe6Znrq9UqJR_vwqQwn3T88ceL3EnjhTAxFcAqGOCSv18Jz_l6MZayL7fAck3JcOMfm0zDFY-xC-YSfH8tcBSrrEoFn1EpRti9rzRTH9Hdsa5ogdVV4WS-3l-uCDjI0yqfodRo"
+            }
+        };
+        let executorWithInvalidEvent = new Executor(invalidEvent,{});
+        let isEventValid = executorWithInvalidEvent.verifyEvent();
+        expect(isEventValid).to.equal(false);
+    });
+
+    it("Verify executor method returns false when report name is not Adherence", async() => {
+        let invalidEvent = {
+            "reportName" : "ABC",
+            "evolveAuth": {
+                "token" : "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiJ1c2VyOjExZThjMmQyLTY2OGEtNGFlMC05MzVmLTAyNDJhYzExMDAwMyIsInJvbGUiOnsibGVnYWN5SWQiOiJBZG1pbmlzdHJhdG9yIiwic2Vjb25kYXJ5Um9sZXMiOlt7ImlkIjoiMTFlOTA5MDAtN2NmZS0yMDcwLTkzNzUtMDI0MmFjMTEwMDA1IiwibGFzdFVwZGF0ZVRpbWUiOjE2MTIyMDM0NDEwMDB9XSwiaWQiOiIxMWU4YzJkMi02MzhjLWMyNTAtYjk5NC0wMjQyYWMxMTAwMDIiLCJsYXN0VXBkYXRlVGltZSI6MTY2NTA1MTQ5NDUyN30sImljQWdlbnRJZCI6IjMyNTgxMyIsImlzcyI6Imh0dHBzOlwvXC9hdXRoLnRlc3QubmljZS1pbmNvbnRhY3QuY29tIiwiZ2l2ZW5fbmFtZSI6IkVtaWx5IiwiYXVkIjoiaW5Db250YWN0IEV2b2x2ZUBpbkNvbnRhY3QuY29tIiwiaWNTUElkIjoiMTA0ODIiLCJpY0JVSWQiOjExMjYzMjE1NzgsIm5hbWUiOiJwbS5rZXBsZXIuYWRtaW5pc3RyYXRvckB3Zm9zYWFzLmNvbSIsInRlbmFudElkIjoiMTFlOGMyZDItNWZiZS1jYTYwLTg1MjQtMDI0MmFjMTEwMDA5IiwiZXhwIjoxNjY1MTQzMTQ2LCJpYXQiOjE2NjUxMzk1NDYsImZhbWlseV9uYW1lIjoiU21pdGgiLCJ0ZW5hbnQiOiJwZXJtX3BtX2tlcGxlcl90ZW5hbnQyNDEzNDg0MCIsInZpZXdzIjp7fSwiaWNDbHVzdGVySWQiOiJUTzMyIn0.Z-vcxglWSK83V7w0fxAKSNOjWktdH4FVb1fGPSe6Znrq9UqJR_vwqQwn3T88ceL3EnjhTAxFcAqGOCSv18Jz_l6MZayL7fAck3JcOMfm0zDFY-xC-YSfH8tcBSrrEoFn1EpRti9rzRTH9Hdsa5ogdVV4WS-3l-uCDjI0yqfodRo"
+            }
+        };
+        let executorWithInvalidEvent = new Executor(invalidEvent,{});
+        let isEventValid = executorWithInvalidEvent.verifyEvent();
+        expect(isEventValid).to.equal(false);
+    });
+
+    it("Verify executor method returns false when date range is incorrect", async() => {
+        let invalidEvent = {
+            "reportName" : "ABC",
+            "reportDateRange" : {
+                "from": "2022-10-03"
+            },
+            "evolveAuth": {
+                "token" : "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiJ1c2VyOjExZThjMmQyLTY2OGEtNGFlMC05MzVmLTAyNDJhYzExMDAwMyIsInJvbGUiOnsibGVnYWN5SWQiOiJBZG1pbmlzdHJhdG9yIiwic2Vjb25kYXJ5Um9sZXMiOlt7ImlkIjoiMTFlOTA5MDAtN2NmZS0yMDcwLTkzNzUtMDI0MmFjMTEwMDA1IiwibGFzdFVwZGF0ZVRpbWUiOjE2MTIyMDM0NDEwMDB9XSwiaWQiOiIxMWU4YzJkMi02MzhjLWMyNTAtYjk5NC0wMjQyYWMxMTAwMDIiLCJsYXN0VXBkYXRlVGltZSI6MTY2NTA1MTQ5NDUyN30sImljQWdlbnRJZCI6IjMyNTgxMyIsImlzcyI6Imh0dHBzOlwvXC9hdXRoLnRlc3QubmljZS1pbmNvbnRhY3QuY29tIiwiZ2l2ZW5fbmFtZSI6IkVtaWx5IiwiYXVkIjoiaW5Db250YWN0IEV2b2x2ZUBpbkNvbnRhY3QuY29tIiwiaWNTUElkIjoiMTA0ODIiLCJpY0JVSWQiOjExMjYzMjE1NzgsIm5hbWUiOiJwbS5rZXBsZXIuYWRtaW5pc3RyYXRvckB3Zm9zYWFzLmNvbSIsInRlbmFudElkIjoiMTFlOGMyZDItNWZiZS1jYTYwLTg1MjQtMDI0MmFjMTEwMDA5IiwiZXhwIjoxNjY1MTQzMTQ2LCJpYXQiOjE2NjUxMzk1NDYsImZhbWlseV9uYW1lIjoiU21pdGgiLCJ0ZW5hbnQiOiJwZXJtX3BtX2tlcGxlcl90ZW5hbnQyNDEzNDg0MCIsInZpZXdzIjp7fSwiaWNDbHVzdGVySWQiOiJUTzMyIn0.Z-vcxglWSK83V7w0fxAKSNOjWktdH4FVb1fGPSe6Znrq9UqJR_vwqQwn3T88ceL3EnjhTAxFcAqGOCSv18Jz_l6MZayL7fAck3JcOMfm0zDFY-xC-YSfH8tcBSrrEoFn1EpRti9rzRTH9Hdsa5ogdVV4WS-3l-uCDjI0yqfodRo"
+            }
+        };
+        let executorWithInvalidEvent = new Executor(invalidEvent,{});
+        let isEventValid = executorWithInvalidEvent.verifyEvent();
+        expect(isEventValid).to.equal(false);
+    });
+
+    it("Verify executor method returns false when query is not present", async() => {
+        let invalidEvent = {
+            "reportName" : "ABC",
+            "reportDateRange" : {
+                "from": "2022-10-03",
+                "to" : "2022-10-03"
+            },
+            "evolveAuth": {
+                "token" : "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiJ1c2VyOjExZThjMmQyLTY2OGEtNGFlMC05MzVmLTAyNDJhYzExMDAwMyIsInJvbGUiOnsibGVnYWN5SWQiOiJBZG1pbmlzdHJhdG9yIiwic2Vjb25kYXJ5Um9sZXMiOlt7ImlkIjoiMTFlOTA5MDAtN2NmZS0yMDcwLTkzNzUtMDI0MmFjMTEwMDA1IiwibGFzdFVwZGF0ZVRpbWUiOjE2MTIyMDM0NDEwMDB9XSwiaWQiOiIxMWU4YzJkMi02MzhjLWMyNTAtYjk5NC0wMjQyYWMxMTAwMDIiLCJsYXN0VXBkYXRlVGltZSI6MTY2NTA1MTQ5NDUyN30sImljQWdlbnRJZCI6IjMyNTgxMyIsImlzcyI6Imh0dHBzOlwvXC9hdXRoLnRlc3QubmljZS1pbmNvbnRhY3QuY29tIiwiZ2l2ZW5fbmFtZSI6IkVtaWx5IiwiYXVkIjoiaW5Db250YWN0IEV2b2x2ZUBpbkNvbnRhY3QuY29tIiwiaWNTUElkIjoiMTA0ODIiLCJpY0JVSWQiOjExMjYzMjE1NzgsIm5hbWUiOiJwbS5rZXBsZXIuYWRtaW5pc3RyYXRvckB3Zm9zYWFzLmNvbSIsInRlbmFudElkIjoiMTFlOGMyZDItNWZiZS1jYTYwLTg1MjQtMDI0MmFjMTEwMDA5IiwiZXhwIjoxNjY1MTQzMTQ2LCJpYXQiOjE2NjUxMzk1NDYsImZhbWlseV9uYW1lIjoiU21pdGgiLCJ0ZW5hbnQiOiJwZXJtX3BtX2tlcGxlcl90ZW5hbnQyNDEzNDg0MCIsInZpZXdzIjp7fSwiaWNDbHVzdGVySWQiOiJUTzMyIn0.Z-vcxglWSK83V7w0fxAKSNOjWktdH4FVb1fGPSe6Znrq9UqJR_vwqQwn3T88ceL3EnjhTAxFcAqGOCSv18Jz_l6MZayL7fAck3JcOMfm0zDFY-xC-YSfH8tcBSrrEoFn1EpRti9rzRTH9Hdsa5ogdVV4WS-3l-uCDjI0yqfodRo"
+            }
+        };
+        let executorWithInvalidEvent = new Executor(invalidEvent,{});
+        let isEventValid = executorWithInvalidEvent.verifyEvent();
+        expect(isEventValid).to.equal(false);
+    });
+
+    it("Verify executor method returns empty list when no users are fetched", async() => {
+        let response = mockAPIResponse;
+        response.users = [];
+        performGetRequestToCXoneStub.resolves(JSON.stringify(response));
+        let userIds = await executor.getUsersFromUH();
+        expect(userIds.length).to.equal(0);
+    });
+
+    it("Verify executor method returns non empty list when users are present", async() => {
+        performGetRequestToCXoneStub.resolves(JSON.stringify(mockAPIResponse));
+        let userIds = await executor.getUsersFromUH();
+        expect(userIds.length).to.equal(2);
+    });
+
+    it("Verify executor method returns empty list when users API fails", async() => {
+        let response={};
+        response.tenant = mockAPIResponse.tenant;
+        performGetRequestToCXoneStub.resolves(JSON.stringify(response));
+        let userIds = await executor.getUsersFromUH();
+        expect(userIds.length).to.equal(0);
+    });
+
+});
+
+describe('WFM RTA export report  failure test cases', function () {
+    this.timeout(3000);
+    var getMockEvent = function (eventData) {
+        return eventData;
+    };
+
+    beforeEach(function () {
+        mockEvent = getMockEvent(JSON.parse(JSON.stringify(require('./mocks/mockEvent.json'))));
+        process.env.SERVICE_URL = "";
+    });
+
+    it("Report export Fail without host", done => {
+        LambdaTester(Handler)
+            .event(mockEvent)
+            .expectError(error => {
+                expect(error.message).to.equal("FAILED TO VALIDATE HOST");
                 done();
             })
             .catch(done);
